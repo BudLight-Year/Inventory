741dbc087b6897643722fc18d055809e6b47f163
BudLight-Year <totallysweethobo@gmail.com>
2023-11-25 23:14:49 -0700

    some commit

---
+++ src/WeaponSort.java
@@ -0,0 +1,17 @@
+package src;
+
+import java.util.Comparator;
+
+public class WeaponSort implements Comparator<Weapon> {
+
+
+    @Override
+    public int compare(Weapon o1, Weapon o2) {
+        if (o1.getDamage() > o2.getDamage()){
+            return 1;
+        }else if(o1.getDamage() < o2.getDamage()){
+            return -1;
+        }
+        return 0;
+    }
+}
